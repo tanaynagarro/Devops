33a318ec8e575d64699cd8f57a4005494eb7aae5
tanaynagarro <84174060+tanaynagarro@users.noreply.github.com>
2021-05-14 15:26:24 +0530

    Add files via upload

---
+++ display.java
@@ -0,0 +1,11 @@
+package HelloWorld;
+
+public class display {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is a jenkin project");
+	}
+
+}
